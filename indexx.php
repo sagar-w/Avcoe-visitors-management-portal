15c280bd0ea256614ec7aef57b58ebfafdb4f606
Sagar Waghmare <78874553+sagar-w@users.noreply.github.com>
2021-07-29 22:05:53 +0530

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -24,7 +24,7 @@ if(isset($_POST['login']))
 <html lang="en">
 
 <head>
-    <!-- Required meta tags-->
+  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="au theme template">
